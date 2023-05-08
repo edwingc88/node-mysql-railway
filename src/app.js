a8493c95d3006c0d8ad35540855ede793d34177d
unknown <edwingc88@gmail.com>
2023-05-08 06:28:40 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import { pool } from './db.js'
+import {PORT} from './config.js'
+
+const app = express()
+
+app.get('/',async (req,res)=>{
+    const [rows]= await pool.query('SELECT * from users')
+    res.send(rows)
+})
+app.get('/employess',(req,res)=>res.send('obteniendo empleados'))
+app.post('/employess',(req,res)=>res.send('creando empleados'))
+app.put('/employess',(req,res)=>res.send('actualizando empleados'))
+app.delete('/employess',(req,res)=>res.send('eliminando empleados'))
+
+app.get('/ping',async (req,res)=>{
+    const [result] = await pool.query(`SELECT "hello world" as RESULT`);
+    console.log(result[0])
+    res.json(result[0])
+})
+
+app.get('/create',async (req,res)=>{
+    const result = await pool.query(`INSERT INTO users(name) VALUES ("John")`);
+    console.log(result)
+    res.json(result)
+})
+
+app.listen(PORT)
+
+/*console.log("Server  GG  port 3000")*/
