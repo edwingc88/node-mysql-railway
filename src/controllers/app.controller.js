e9b7be94d54556b4de57a137de662dae8c1aa711
unknown <edwingc88@gmail.com>
2023-05-09 09:30:20 -0400

    api terminada con variable env y demas consultas

--- src/controllers/app.controller.js
+++ src/controllers/app.controller.js
@@ -1,12 +1,11 @@
-import { pool } from '../db.js'
+/*import { pool } from '../db.js'*/
 
 
 export const users = async (req,res)=>{
-    const [rows]= await pool.query('SELECT * from users')
-    res.send(rows)
+    res.send("Bienvenido a la API de Empleados : GET & POST /api/employees --- GET & PATCH & DELETE /api/employees/:id ")
 }
 
-export const ping= async (req,res)=>{
+/*export const ping= async (req,res)=>{
     const [result] = await pool.query(`SELECT "Pong" as RESULT`);
     console.log(result[0])
     res.json(result[0])
@@ -16,4 +15,4 @@ export const createUser= async (req,res)=>{
     const result = await pool.query(`INSERT INTO users(name) VALUES ("John")`);
     console.log(result)
     res.json(result)
-}
+}*/
--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,13 +1,13 @@
 import {Router} from 'express'
-import { ping , createUser, users  } from '../controllers/app.controller.js'
+import { users } from '../controllers/app.controller.js'
 
 const router = Router()
 
 router.get('/',users)
 
-router.get('/ping',ping)
+/*router.get('/ping',ping)
 
-router.get('/create',createUser)
+router.get('/create',createUser)*/
 
 
 export default router
